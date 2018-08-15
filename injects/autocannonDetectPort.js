ddf5a154c60b9177b2a1a9112c0ea69784ddf67c
Renée Kooi <renee@kooi.me>
2018-08-15 15:46:13 +0200

    Revert "Avoid the half open stuff"
    This reverts commit 9ef42709701f5620f103319191cd22fba64ff03a.

--- injects/autocannonDetectPort.js
+++ injects/autocannonDetectPort.js
@@ -3,12 +3,14 @@
 const onListen = require('on-net-listen')
 const net = require('net')
 
-const socket = net.connect(process.env.AUTOCANNON_SOCKET)
+const socket = net.connect(process.env.AUTOCANNON_SOCKET, {
+  allowHalfOpen: true
+})
 
 onListen(function (addr) {
   this.destroy()
   const port = Buffer.from(addr.port + '')
-  socket.write(port)
+  socket.end(port)
 })
 
 // `nitm` catches the SIGINT so we write it to a file descriptor instead
